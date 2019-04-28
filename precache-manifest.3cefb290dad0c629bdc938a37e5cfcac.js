eeeeefa8efbfaf06e506a0e8d60623da51fdda88
Pooja Saxena <pooja335@gmail.com>
2019-04-28 14:03:53 -0500

    Updates

---
+++ precache-manifest.3cefb290dad0c629bdc938a37e5cfcac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81db1cf9bfd1fae6e384339fe3cc1b31",
+    "url": "/got-bingo/index.html"
+  },
+  {
+    "revision": "16c5a00e4b09e32fc975",
+    "url": "/got-bingo/static/css/main.2a494b20.chunk.css"
+  },
+  {
+    "revision": "e8fb0337a08740ae382c",
+    "url": "/got-bingo/static/js/2.c4c3f6a9.chunk.js"
+  },
+  {
+    "revision": "16c5a00e4b09e32fc975",
+    "url": "/got-bingo/static/js/main.bec07470.chunk.js"
+  },
+  {
+    "revision": "0c869ea4417913c7f6c6",
+    "url": "/got-bingo/static/js/runtime~main.cdf91135.js"
+  }
+]);
